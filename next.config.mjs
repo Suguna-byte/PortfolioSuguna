66a07d665d90ff28b5f7e1d9ec565b9143dd06ce
Suguna-byte <jayarajansuguna@gmail.com>
2026-05-18 10:41:55 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      { protocol: "https", hostname: "**.substack.com" },
+      { protocol: "https", hostname: "substackcdn.com" },
+      { protocol: "https", hostname: "**.substackcdn.com" },
+    ],
   },
 }
-
 export default nextConfig
